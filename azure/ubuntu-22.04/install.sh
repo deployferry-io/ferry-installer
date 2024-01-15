a51985f4d8fb987b6af919974f1aecd13842a485
ethanwaldieferry <ethan@thirdrails.io>
2024-01-15 15:59:19 +0000

    making the ubuntu installer more hands off

--- azure/ubuntu-22.04/install.sh
+++ azure/ubuntu-22.04/install.sh
@@ -7,15 +7,13 @@ done
 echo $KEY
 
 ## update and install packages
-apt-get update
-apt-get install python3 python3-pip unzip
+apt-get update && apt-get install -y python3 python3-pip unzip
 
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
-apt-get update
-apt-get install moby-engine
+apt-get update && apt-get install -y moby-engine
 
 touch /etc/docker/daemon.json
 
@@ -25,8 +23,7 @@ echo '{
 
 systemctl restart docker
 
-apt-get update
-apt-get install aziot-edge
+apt-get update && apt-get install -y aziot-edge
 
 mkdir -p /var/aziot/secrets
 mkdir -p /var/secrets/aziot/identityd
