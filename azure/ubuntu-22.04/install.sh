c5994106eb63743d3828054f5ebccb1a8d21e62c
ethanwaldieferry <ethan@thirdrails.io>
2024-02-19 16:15:56 -0500

    removing a sudo ref from the installer script

--- azure/ubuntu-22.04/install.sh
+++ azure/ubuntu-22.04/install.sh
@@ -10,7 +10,7 @@ echo $KEY
 apt-get update && apt-get install -y python3 python3-pip unzip
 
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 apt-get update && apt-get install -y moby-engine
